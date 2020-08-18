2f49c99209f4e1fc1d42e06d72879e204fb44564
Pedro Mangabeira <pedro.mangabeiralindekrantz@gmail.com>
2020-08-18 19:37:59 +0100

    fix: 🐛 fix config

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,7 @@
 import Config
 
-config :spandex_newrelic, :api_key, ""
-config :spandex_newrelic, :api_url, "https://trace-api.newrelic.com/trace/v1"
+config :spandex_newrelic,
+  api_key: "",
+  api_url: "https://trace-api.newrelic.com/trace/v1"
 
 import_config "./#{Mix.env()}.exs"
